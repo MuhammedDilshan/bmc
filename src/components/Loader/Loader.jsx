8742b766a348b7d3d3f2d07fb9f4c1300ddbd4ba
dilshanmambadan <dilshanmambadan@gmail.com>
2024-08-17 14:44:40 +0530

    dilshan

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 
 const Loader = () => {
-  return (
-    <div className="loader">
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-  );
+  return <span class="loader"></span>;
 };
 
 export default Loader;
